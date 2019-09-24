99b3ad77dc819bd9f063f1cbeb78054457ed95e6
Michiel Vlootman <michiel@vlootman.com>
2019-09-24 22:06:24 +0200

    fix output format label added gitignore

---
+++ main.v
@@ -0,0 +1,28 @@
+import bench
+import time
+
+fn main(){
+        mut b := bench.Benchmark{
+                warmup_secs: 2,
+                bench_secs: 5
+        }
+        
+        b.bench('sleep short', sleep_short)    
+        b.bench('sleep shorter', sleep_shorter)   
+        b.bench('alloc_some', alloc_some)   
+}
+
+fn sleep_short(){
+        time.sleep_ms(10)
+}
+
+fn sleep_shorter(){
+        time.sleep_ms(1)
+}
+
+fn alloc_some(){
+        mut s_arr := []string
+        for i := 0; i < 100000; i++{
+                s_arr << i.str()
+        }
+}
